a031a81383422d69ade115b3fd0c204ddb25be47
Sean Collins <scollinspt@tutanota.com>
2023-04-26 16:35:09 -0400

    Week 3 exercises

---
+++ 3-1-4-Final-Code/UsingDeckClass/UsingDeckClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UsingDeckClass
+{
+    /// <summary>
+    /// Classes and Objects lecture code
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Demonstrates using a Deck class
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // declare and print a deck of cards
+            Deck deck = new Deck();
+            //deck.Print();
+
+            // access and print Empty property
+            //bool isEmpty = deck.Empty;
+            //Console.WriteLine("Deck empty: " + isEmpty);
+            Console.WriteLine("Deck empty: " + deck.Empty);
+        }
+    }
+}
---
+++ 3-1-5-Final-Code/UsingDeckClass/UsingDeckClass/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace UsingDeckClass
+{
+    /// <summary>
+    /// Classes and Objects lecture code
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Demonstrates using a Deck class
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // declare and print a deck of cards
+            Deck deck = new Deck();
+            deck.Print();
+
+            // access and print Empty property
+            //bool isEmpty = deck.Empty;
+            //Console.WriteLine("Deck empty: " + isEmpty);
+            //Console.WriteLine("Deck empty: " + deck.Empty);
+
+            // shuffle and print deck
+            //Console.WriteLine();
+            //deck.Shuffle();
+            //deck.Print();
+
+            // cut and print deck
+            //Console.WriteLine();
+            //deck.Cut(26);
+            //deck.Print();
+
+            // take and print the top card from the deck
+            Console.WriteLine();
+            deck.TakeTopCard();
+            Card card = deck.TakeTopCard();
+            Console.WriteLine(card.Rank + " of " + card.Suit);
+        }
+    }
+}
